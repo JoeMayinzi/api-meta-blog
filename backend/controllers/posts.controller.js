2936e5508023c08c466192cec5b062187be44769
Joe mayinzi <joemayinzi@gmail.com>
2024-04-16 15:40:41 +0200

    Created SETPOSTS controller

--- backend/controllers/posts.controller.js
+++ backend/controllers/posts.controller.js
@@ -10,4 +10,17 @@ const GetPosts = async (req, res) => {
   res.status(200).json(posts);
 };
 
-module.exports = { GetPosts };
+const SetPosts = async (req, res) => {
+  if (!req.body) {
+    res.status(400).json({ message: "Veillez ajouter un article" });
+  }
+
+  const post = PostModel.create({
+    title: req.title,
+    content: req.body.content,
+  });
+
+  res.status(200).json(post);
+};
+
+module.exports = { GetPosts, SetPosts };
--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -1,12 +1,9 @@
 const express = require("express");
-const { GetPosts } = require("../controllers/posts.controller");
+const { GetPosts, SetPosts } = require("../controllers/posts.controller");
 const Router = express.Router();
 
 Router.get("/", GetPosts);
-
-Router.post("/", (req, res) => {
-  res.status(200).json({ message: req.body.message });
-});
+Router.post("/", SetPosts);
 
 Router.delete("/:id", (req, res) => {
   res.json({ message: "Element supprimé avec succès" + " " + req.params.id });
