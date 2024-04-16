d2de328003e6c75f24b9fb3d2eae2cbf2810d921
Joe mayinzi <joemayinzi@gmail.com>
2024-04-16 14:00:56 +0200

    Created PUT route successfuly

--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -19,4 +19,10 @@ Router.delete("/:id", (req, res) => {
   res.json({ message: "Element supprimé avec succès" + " " + req.params.id });
 });
 
+Router.put("/:id", (req, res) => {
+  res
+    .status(200)
+    .json({ message: "element modifié avec succès" + " " + req.params.id });
+});
+
 module.exports = Router;
